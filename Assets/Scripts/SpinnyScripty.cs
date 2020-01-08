09decfd95537dc63f6fb5f58a548150d499fae86
Martin B Omdahl <martinbrom@hotmail.no>
2020-01-08 13:29:15 +0100

    Made a cube that spins
    It spins real fast (i used code to do it)
    Please note that the cube is stretched, making it not a cube.

---
+++ Assets/Scripts/SpinnyScripty.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnyScripty : MonoBehaviour
+{
+    float spinSpeed = -1;
+
+    ConstantForce constForce;
+    void Start()
+    {
+        constForce = GetComponent<ConstantForce>();
+    }
+
+    void Update()
+    {
+        //constForce.relativeTorque = new Vector3(0, 0, constForce.relativeTorque.z - Time.deltaTime);
+
+        spinSpeed -= Time.deltaTime;
+        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+    }
+}
